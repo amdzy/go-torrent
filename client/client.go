1f5c1d30c8d6e7db78b6526e5a73c55a6b7b719f
Ahmad Zabadany <amdzy@proton.me>
2024-10-01 14:26:10 +0400

    feat: implement client

---
+++ client/client.go
@@ -0,0 +1,124 @@
+package client
+
+import (
+	"amdzy/go-torrent/bitfield"
+	"amdzy/go-torrent/handshake"
+	"amdzy/go-torrent/message"
+	"amdzy/go-torrent/peers"
+	"bytes"
+	"fmt"
+	"net"
+	"time"
+)
+
+type Client struct {
+	Conn     net.Conn
+	Choked   bool
+	Bitfield bitfield.Bitfield
+	peer     peers.Peer
+	infoHash [20]byte
+	peerID   [20]byte
+}
+
+func completeHandshake(conn net.Conn, infoHash, peerId [20]byte) (*handshake.Handshake, error) {
+	conn.SetDeadline(time.Now().Add(5 * time.Second))
+	defer conn.SetDeadline(time.Time{})
+
+	req := handshake.New(infoHash, peerId)
+	_, err := conn.Write(req.Serialize())
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := handshake.Read(conn)
+	if err != nil {
+		return nil, err
+	}
+
+	if !bytes.Equal(res.InfoHash[:], infoHash[:]) {
+		return nil, fmt.Errorf("expected info hash %x but got %x", res.InfoHash, infoHash)
+	}
+	return res, nil
+}
+
+func recvBitfield(conn net.Conn) (bitfield.Bitfield, error) {
+	conn.SetDeadline(time.Now().Add(5 * time.Second))
+	defer conn.SetDeadline(time.Time{})
+
+	msg, err := message.Read(conn)
+	if err != nil {
+		return nil, err
+	}
+
+	if msg == nil {
+		err := fmt.Errorf("expected bitfield but got %s", msg)
+		return nil, err
+	}
+
+	if msg.ID != message.MsgBitfield {
+		err := fmt.Errorf("expected bitfield but got ID %d", msg.ID)
+		return nil, err
+	}
+
+	return msg.Payload, nil
+}
+
+func New(peer peers.Peer, infoHash, peerId [20]byte) (*Client, error) {
+	conn, err := net.DialTimeout("tcp", peer.String(), 3*time.Second)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = completeHandshake(conn, infoHash, peerId)
+	if err != nil {
+		conn.Close()
+		return nil, err
+	}
+
+	bf, err := recvBitfield(conn)
+	if err != nil {
+		conn.Close()
+		return nil, err
+	}
+
+	return &Client{
+		Conn:     conn,
+		Choked:   true,
+		Bitfield: bf,
+		peer:     peer,
+		infoHash: infoHash,
+		peerID:   peerId,
+	}, nil
+}
+
+func (c *Client) Read() (*message.Message, error) {
+	msg, err := message.Read(c.Conn)
+	if err != nil {
+		return nil, err
+	}
+	return msg, nil
+}
+
+func (c *Client) SendRequest(index, begin, length int) error {
+	req := message.FormatRequest(index, begin, length)
+	_, err := c.Conn.Write(req.Serialize())
+	return err
+}
+
+func (c *Client) SendInterested() error {
+	msg := message.Message{ID: message.MsgInterested}
+	_, err := c.Conn.Write(msg.Serialize())
+	return err
+}
+
+func (c *Client) SendNotInterested() error {
+	msg := message.Message{ID: message.MsgNotInterested}
+	_, err := c.Conn.Write(msg.Serialize())
+	return err
+}
+
+func (c *Client) SendUnchoke() error {
+	msg := message.Message{ID: message.MsgUnchoke}
+	_, err := c.Conn.Write(msg.Serialize())
+	return err
+}
